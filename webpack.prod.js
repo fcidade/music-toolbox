91b3cd6c280da07f858d86b7c9178806b35f394d
Francisco Cidade <franciscocidadehomemjunior@gmail.com>
2022-06-19 17:43:32 -0300

    build script

---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    'react-router-dom': 'ReactRouterDOM'
+  }
+})
